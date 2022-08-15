200700a0d43d44d5f9a9371095be3ecd66c42ca6
Maxim Kanov <110252723+Homsers@users.noreply.github.com>
2022-08-15 19:48:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿Console.WriteLine("Enter a number");
-int x1 = int.Parse(Console.ReadLine());
-int y1 = int.Parse(Console.ReadLine());
-int z1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter a second number");
-int x2 = int.Parse(Console.ReadLine());
-int y2 = int.Parse(Console.ReadLine());
-int z2 = int.Parse(Console.ReadLine());
+int number0 = int.Parse(Console.ReadLine());
 
-
-double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2-y1, 2)+ Math.Pow(z2-z1, 2));
-Console.WriteLine($"Distance between 2 points is {distance}");
+for (int i=1; i<= number0; i++)
+{
+    Console.Write(Math.Pow(i,3)+", ");
+}
